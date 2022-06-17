54f88401422fd9a9e7946db103075fac2838ac19
Jayden Chan <jaydencn7@gmail.com>
2022-06-17 14:01:58 -0700

    JC: Greatly improved Git integrations

--- lua/base46/integrations/git.lua
+++ lua/base46/integrations/git.lua
@@ -1,12 +1,27 @@
 return function(colors)
     return {
-        -- gitsigns.nvim
-        DiffAdd = { fg = colors.green },
-        DiffAdded = { fg = colors.green },
-        DiffChange = { fg = colors.light_grey },
+        -- Diff
+        DiffAdd = { bg = '#1e3425' },
+        DiffAdded = { bg = '#1e3425' },
+        DiffChange = { bg = colors.black },
         DiffChangeDelete = { fg = colors.red },
         DiffModified = { fg = colors.orange },
-        DiffDelete = { fg = colors.red },
-        DiffRemoved = { fg = colors.red },
+        DiffDelete = { bg = '#3b2021' },
+        DiffRemoved = { fg = colors.red, bg = '#3b2021' },
+        DiffText = { bg = colors.light_grey },
+
+        -- diffview.nvim
+        DiffviewFilePanelTitle = { fg = colors.cyan, bold = true },
+        DiffviewDiffAddAsDelete = { bg = '#3b2021' },
+
+        -- GitSigns.nvim
+        GitSignsAdd = { fg = colors.green },
+        GitSignsAdded = { fg = colors.green },
+        GitSignsChange = { fg = colors.light_grey },
+        GitSignsChangeDelete = { fg = colors.red },
+        GitSignsModified = { fg = colors.orange },
+        GitSignsDelete = { fg = colors.red },
+        GitSignsRemoved = { fg = colors.red },
+        GitSignsText = { bg = colors.light_grey },
     }
 end
--- lua/base46/integrations/neogit.lua
+++ lua/base46/integrations/neogit.lua
@@ -7,8 +7,9 @@ return function(colors)
         NeogitDiffAddHighlight = { bg = colors.one_bg, fg = colors.green },
         NeogitDiffDeleteHighlight = { bg = colors.one_bg, fg = colors.red },
         NeogitDiffContextHighlight = { bg = colors.black2, fg = colors.light_grey },
-        NeogitHunkHeader = { fg = colors.greyfg2, bg= colors.black },
+        NeogitHunkHeader = { fg = colors.greyfg2, bg = colors.black },
         NeogitHunkHeaderHighlight = { fg = colors.greyfg2, bg = colors.black2 },
-        NeogitCommitViewHeader = { bg = colors.one_bg3, fg = colors.white, bold = true }
+        NeogitCommitViewHeader = { bg = colors.one_bg3, fg = colors.white, bold = true },
+        NeogitObjectId = { fg = colors.purple }
     }
 end
